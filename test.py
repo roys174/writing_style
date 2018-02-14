ca72bcfc0f5b5cd50a1be1594f442f2d1f78cd89
Roy Schwartz <roysch@164.7.208.128.dynr.cs.washington.edu>
2018-02-14 10:19:36 -0800

    test.py supports for output file

--- test.py
+++ test.py
@@ -7,9 +7,14 @@ import pickle
 
 
 def main():
+    ofile = None
+    text_ifile = None
     if (len(sys.argv) < 3):
-        print ("Usage:",sys.argv[0],"<if> <model if>")
+        print ("Usage:",sys.argv[0],"<if> <model if> <text ifile - optional> <ofile - optional>")
         return -1
+    elif (len(sys.argv) > 4):
+        text_ifile = sys.argv[3]
+        ofile = sys.argv[4]
 
     clf = pickle.load(open(sys.argv[2], 'r'))
     n_feats = len(clf.coef_[0])
@@ -20,6 +25,9 @@ def main():
     
     logistic_regression_tools.evaluate(test,labels)
     
-
+    if ofile is not None:
+        with open(ofile, 'w') as ofh, open(text_ifile) as ifh:
+            for l, test, label in zip(ifh, test, labels):
+                ofh.write("{}   {}  {}".format(test, label, l))
 
 sys.exit(main())
